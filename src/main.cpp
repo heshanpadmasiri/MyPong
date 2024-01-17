b232c2c4010dab37af020f07046b95af8ef85771
Heshan Padmasiri <hpheshan@gmail.com>
2024-01-17 20:19:38 +0530

    Render bat and ball

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include "raylib.h"
+#include <cstdio>
 
-auto main(int argc, char *argv[]) -> int {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 450
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
+#define BAT_WIDTH 200
+#define BAT_HEIGHT 20
+#define BAT_LINE 400
 
-    CloseWindow();
+#define BALL_RADIUS 15
+#define BALL_COLOR RED
 
+#define BAT_COLOR BLACK
 
-    return 0;
+struct GameState {
+  float ballX;
+  int ballY;
+  float batX;
+};
+
+void drawFrame(GameState state) {
+  {
+    // draw bat
+    float x_corner = state.batX - (BAT_WIDTH / 2);
+    Rectangle rec = {x_corner, BAT_LINE, BAT_WIDTH, BAT_HEIGHT};
+    DrawRectangleRec(rec, BAT_COLOR);
+  }
+  {
+    // draw ball
+    DrawCircle(state.ballX, state.ballY, BALL_RADIUS, BALL_COLOR);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+  SetTargetFPS(60);
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "My pong");
+  float centerX = GetScreenWidth() / 2;
+  GameState state { centerX, 50, centerX };
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    drawFrame(state);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
